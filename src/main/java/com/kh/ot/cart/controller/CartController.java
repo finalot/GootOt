75726dce6a8a30ce9dbd0be14bd555337aee0612
wooyejin <52619813+wooyejin@users.noreply.github.com>
2020-04-17 13:31:40 +0900

    영수증 수정

--- src/main/java/com/kh/ot/cart/controller/CartController.java
+++ src/main/java/com/kh/ot/cart/controller/CartController.java
@@ -254,6 +254,17 @@ public class CartController extends HttpServlet {
    		return mv;
 	}
 
+	
+	/**
+	 * @작성일  : 2020. 4. 17.
+	 * @작성자  : 우예진
+	 * @내용    : 영수증 리스트 뿌려주기
+	 * @return
+	 */
+	@RequestMapping("receipt.do")
+	public String receipt() {
+		return "receipt";
+	}
 
 
 
